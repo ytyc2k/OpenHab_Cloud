b6b0fd9d9128febae4642cf8d1cc1b1557ad1f22
Ubuntu <ubuntu@ip-172-31-50-40.ec2.internal>
2019-05-30 10:45:51 -0500

    20190530

---
+++ README.md
@@ -0,0 +1,18 @@
+# Steps
+
+* Setup
+```
+sudo apt update && sudo apt install git -y
+```
+
+* Git clone this repo
+```
+git clone https://bitbucket.org/umbrela/myopenhab-setup-scripts
+```
+
+* Run script
+```
+cd myopenhab-setup-scripts
+chmod +x setup.sh
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,46 @@
+sudo ln -fs /usr/share/zoneinfo/Canada/Central /etc/localtime
+
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install -y build-essential redis-server mongodb nginx python git
+
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get -y install nodejs
+
+cd
+git clone https://github.com/openhab/openhab-cloud.git
+
+cd openhab-cloud
+
+cp ~/myopenhab-setup-scripts/config.json ./config.json
+cp ~/myopenhab-setup-scripts/user.js ./models/user.js
+cp ~/myopenhab-setup-scripts/mailer.js ./mailer.js
+
+npm install
+npm update
+npm install bcrypt time
+
+# SSL
+sudo service nginx stop
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository universe -y
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update
+sudo apt-get install -y certbot python-certbot-nginx
+sudo certbot --nginx -d myopenhab.umbrela.co -m smartsnsz@gmail.com -n --agree-tos
+
+sudo cp ~/umbrela/default /etc/nginx/sites-available/default
+grep -q "sudo /usr/bin/node /home/ubuntu/openhab-cloud/app.js" /etc/rc.local 
+if [[ $? != 0 ]]; then
+  sudo sed -i '$i sudo \/usr\/bin\/node \/home\/ubuntu\/openhab-cloud\/app.js' /etc/rc.local 
+fi
+
+cd ~/openhab-cloud
+
+sudo nohup node app.js &
+sudo service nginx restart
+
+# automate cert renewal
+(crontab -l ; echo "* 7,19 * * * certbot -q renew") | sudo crontab
+
+# show in browser https://myopenhab.umbrela.co
